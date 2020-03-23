013399473bf729db35db130ca280d2c55e7901d0
Ian Ah Po <ian.ahpo@auspost.com.au>
2020-03-23 19:03:03 +1100

    Initial sign up page

---
+++ ui/src/ui/user-signup.page.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import {Box, Button, Heading, Text, TextField} from 'gestalt';
+import {Vertical, VerticalSpacer} from 'gls';
+
+const HeadingContent = () =>
+    <Box display={'flex'} direction={'column'} alignItems={'center'}>
+        <Heading size="sm">Create Profile</Heading>
+        <VerticalSpacer space={12}/>
+        <Text align={'center'}>We only currently support AHPRA registered employees</Text>
+    </Box>;
+
+export class UserSignupPage extends React.Component {
+
+    render() {
+        return <>
+
+            <VerticalSpacer space={32}/>
+            <HeadingContent/>
+            <VerticalSpacer space={32}/>
+
+            <Vertical>
+                <TextField
+                    label="First name"
+                    onChange={() => {
+                    }}
+                    id="first-name"/>
+                <TextField
+                    label="Last name"
+                    onChange={() => {
+                    }}
+                    id="last-name"/>
+                <TextField
+                    label="AHPRA registration number"
+                    onChange={() => {
+                    }}
+                    id="registration-number"
+                />
+                <TextField
+                    label="Mobile number"
+                    onChange={() => {
+                    }}
+                    id="mobile-number"
+                />
+            </Vertical>
+
+            <VerticalSpacer space={32}/>
+
+            <Vertical>
+                <Button color={'red'} text="Register"/>
+                <Text size={'sm'} align={'center'}>Already have an account?</Text>
+            </Vertical>
+        </>;
+    }
+
+}
